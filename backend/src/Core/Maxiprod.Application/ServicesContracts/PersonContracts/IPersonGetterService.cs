2640d7352e5f04bef1e5263901d18de185fed709
Danil Muniz <ca.daniel@outlook.com>
2026-01-10 05:00:38 -0300

    ✨ feat(PersonContract): Implement person interface for CRUD operations.

--- backend/src/Core/Maxiprod.Application/ServicesContracts/PersonContracts/IPersonGetterService.cs
+++ backend/src/Core/Maxiprod.Application/ServicesContracts/PersonContracts/IPersonGetterService.cs
@@ -1,6 +1,20 @@
+using Maxiprod.Domain.Entity;
+
 namespace Maxiprod.Application.ServicesContracts.PersonContracts;
 
+/// <summary>
+/// Service interface for getting person.
+/// </summary>
 public interface IPersonGetterService
 {
-
+    /// <summary>
+    /// Gets a person by ID asynchronously.
+    /// </summary>
+    /// <param name="personId">
+    /// The ID of the person to retrieve.
+    /// </param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains the person if found, otherwise null.
+    /// </returns>
+    public Task<Person?> GetPersonByIdAsync(int personId);
 }
--- backend/src/Core/Maxiprod.Application/ServicesContracts/PersonContracts/IPersonUpdatableService.cs
+++ backend/src/Core/Maxiprod.Application/ServicesContracts/PersonContracts/IPersonUpdatableService.cs
@@ -1,6 +1,23 @@
+using Maxiprod.Application.DTO;
+
 namespace Maxiprod.Application.ServicesContracts.PersonContracts;
 
+/// <summary>
+/// Service interface for updating person.
+/// </summary>
 public interface IPersonUpdatableService
 {
-
+    /// <summary>
+    /// Updates a person asynchronously.
+    /// </summary>
+    /// <param name="personId">
+    /// The ID of the person to update.
+    /// </param>
+    /// <param name="dto">
+    /// The DTO containing the updated person information.
+    /// </param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains true if the person was updated successfully, otherwise false.
+    /// </returns>
+    public Task<bool> UpdatePersonAsync(int personId, PersonDtoUpsert dto);
 }
