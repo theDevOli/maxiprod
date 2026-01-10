854e67c00d83cb7b4ce4d6d6657472ac3f66c875
Danil Muniz <ca.daniel@outlook.com>
2026-01-10 06:21:41 -0300

    ♻️ refactor(ICategoryUpdatableService): Change UpdateCategoryAsync signature.

--- backend/src/Core/Maxiprod.Application/ServicesContracts/CategoryContracts/ICategoryUpdatableService.cs
+++ backend/src/Core/Maxiprod.Application/ServicesContracts/CategoryContracts/ICategoryUpdatableService.cs
@@ -1,4 +1,4 @@
-using Maxiprod.Domain.Entity;
+using Maxiprod.Application.DTO;
 
 namespace Maxiprod.Application.ServicesContracts.CategoryContracts;
 
@@ -7,14 +7,17 @@ namespace Maxiprod.Application.ServicesContracts.CategoryContracts;
 /// </summary>
 public interface ICategoryUpdatableService
 {
-    /// <summary>
-    /// Updates a category.
-    /// </summary>
-    /// <param name="category">
-    /// The category to be updated.
-    /// </param>
-    /// <returns>
-    /// A boolean indicating whether the update was successful.
-    /// </returns>
-    public Task<bool> UpdateCategoryAsync(Category category);
+/// <summary>
+/// Updates a category.
+/// </summary>
+/// <param name="categoryId">
+/// The ID of the category to be updated.
+/// </param>
+/// <param name="categoryDto">
+/// The DTO containing the updated category information.
+/// </param>
+/// <returns>
+/// True if the category was successfully updated; otherwise, false.
+/// </returns>
+    public Task<bool> UpdateCategoryAsync(int categoryId, CategoryDtoUpsert categoryDto);
 }
