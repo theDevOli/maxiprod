21811f11e375ca6d29348b63e7f45a0b36db7121
Daniel Oliveira <ca.daniel@outlook.com>
2026-01-13 10:57:03 -0300

    🔧 chore: package jason

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        port: 5173,
+        host: true,
+        proxy: {
+            "/api": {
+                target: "http://localhost:5000",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, "/v1/api"),
+            },
+        },
+    },
 })
