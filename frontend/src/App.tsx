b57767f3da6806198972f3adcb335226e9d1f421
Daniel Oliveira <ca.daniel@outlook.com>
2026-01-15 10:04:29 -0300

    🔧 chore: Changes on the application core.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import '.styles/App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import NavBar from "./components/NavBar"
+import Footer from "./components/Fotter"
+import { Outlet } from "react-router-dom"
+import { Spinner } from "./components/Spiner"
+import { useLoading } from "./context/LoadingContext"
+import { useEffect } from "react"
+import { useAppState } from "./hooks/useAppState"
 
 function App() {
-  const [count, setCount] = useState(0)
+    const { isLoading } = useLoading()
+    const { restoreState } = useAppState()
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        restoreState()
+    }, [])
+
+    return (
+        <>
+            {isLoading && <Spinner />}
+            <div className="d-flex flex-column min-vh-100">
+                <NavBar />
+
+                <main className="flex-grow-1 w-100 p-5">
+                    <Outlet />
+                </main>
+
+                <Footer />
+            </div>
+        </>
+    )
 }
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,75 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '.styles/index.css'
-import App from './App.tsx'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./styles/index.css"
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import NewCategory from "./pages/Category/NewCategory.tsx"
+import App from "./App.tsx"
+import CategoryDashboard from "./pages/Category/CategoryDashboard.tsx"
+import NewPerson from "./pages/Person/NewPerson.tsx"
+import Home from "./pages/Home/Home.tsx"
+import PersonDashboard from "./pages/Person/PersonDashboard.tsx"
+import NewTransaction from "./pages/Transaction/NewTransaction.tsx"
+import TransactionDashboard from "./pages/Transaction/TransactionDashboard.tsx"
+import CategoryBalance from "./pages/Transaction/CategoryBalance.tsx"
+import PersonBalance from "./pages/Transaction/PersonBalance.tsx"
+import { NotFound } from "./pages/Error/NotFound.tsx"
+import { LoadingProvider } from "./context/LoadingContext.tsx"
+
+const router = createBrowserRouter([
+    {
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "new-category",
+                element: <NewCategory />,
+            },
+            {
+                path: "category-dashboard",
+                element: <CategoryDashboard />,
+            },
+            {
+                path: "new-person",
+                element: <NewPerson />,
+            },
+            {
+                path: "person-dashboard",
+                element: <PersonDashboard />,
+            },
+            {
+                path: "new-transaction",
+                element: <NewTransaction />,
+            },
+            {
+                path: "transaction-dashboard",
+                element: <TransactionDashboard />,
+            },
+            {
+                path: "person-balance",
+                element: <PersonBalance />,
+            },
+            {
+                path: "category-balance",
+                element: <CategoryBalance />,
+            },
+            {
+                path: "*",
+                element: <NotFound />,
+            },
+        ],
+    },
+])
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <LoadingProvider>
+            <RouterProvider router={router} />
+        </LoadingProvider>
+    </StrictMode>
 )
